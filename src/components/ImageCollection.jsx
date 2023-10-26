f7985bb0c3186956ed25e1479f0624f75e200916
Hritik <pypi.pioneer@gmail.com>
2023-10-26 23:45:31 +0530

    spelling mistake corrects'

--- src/components/ImageCollection.jsx
+++ src/components/ImageCollection.jsx
@@ -6,7 +6,7 @@ import ImageProfile from './ImageProfile';
 // it will contain the image fetch from the unsplash API
 export default function ImageCollection() {
     return (
-        <div style={{ margin: '40px 20px' }}>
+        <div style={{ margin: '40px 50px' }}>
             <Grid container spacing={4} justify="center" alignItems="center">
                 <Grid item lg={4} xs={12} sm={6} md={6} >
                     <ImageProfile/>
