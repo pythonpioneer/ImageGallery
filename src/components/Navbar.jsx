e6ed4b8812e63842feea71192ff8e8f6c9cb1dc3
Hritik <pypi.pioneer@gmail.com>
2023-10-27 08:32:20 +0530

    navbar dark mode enabled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import { light } from '@mui/material/styles/createPalette';
+import React, { useState } from 'react';
+import { useTheme } from '../context/themeContext';
 
 
 // component to render navigation bar
 export default function Navbar() {
+
+    // fetch the values from the theme context
+    const { colorPalletes, theme, setTheme } = useTheme();
+    const [modeColor, setModeColor] = useState(colorPalletes.light);
+
+    const handleToggleTheme = () => {
+        if (theme === "dark") {
+            setTheme("light");
+            setModeColor(colorPalletes.light);
+        }
+        else {
+            setTheme("dark");
+            setModeColor(colorPalletes.dark);
+        }
+    }
+    console.log(modeColor)
+
+
+    // return the jsx to render the content
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+            <nav className="navbar navbar-expand-lg navbar-light sticky-top" style={{ backgroundColor: modeColor.background }}>
                 <div className="container"> {/* Wrap the content in a container */}
-                    <a className="navbar-brand" style={{ fontFamily: "Pattaya", fontSize: "30px", fontWeight: "bold", marginBottom: "10px" }} href="#">
+                    <a className="navbar-brand" style={{ fontFamily: "Pattaya", fontSize: "30px", fontWeight: "bold", marginBottom: "10px", color: modeColor.text }} href="#">
                         Image Gallery
                     </a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,31 +36,31 @@ export default function Navbar() {
                     </button>
                     <div className="collapse navbar-collapse justify-content-center ml-4" id="navbarSupportedContent"> {/* Add justify-content-center */}
                         <form className="form-inline my-2 my-lg-0"> {/* Add mr-2 for right margin */}
-                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style={{ borderRadius: "10px" }} />
+                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style={{ borderRadius: "10px", backgroundColor: theme == "dark" ? "#4F4F4F" : "#FFFFFF" }} />
                         </form>
-                        <ul className="navbar-nav mr-5">
+                        <ul className="navbar-nav mr-5" >
                             <li className="nav-item active ml-3">
-                                <a className="nav-link font-weight-bold" href="#">
+                                <a className="nav-link font-weight-bold" href="#" style={{ color: modeColor.text }}>
                                     Explore
                                 </a>
                             </li>
                             <li className="nav-item active ml-3">
-                                <a className="nav-link font-weight-bold" href="#">
+                                <a className="nav-link font-weight-bold" href="#" style={{ color: modeColor.text }}>
                                     Collection
                                 </a>
                             </li>
                             <li className="nav-item active ml-3">
-                                <a className="nav-link font-weight-bold" href="#" id="navbarDropdown" role="button" aria-expanded="false">
+                                <a className="nav-link font-weight-bold" href="#" id="navbarDropdown" role="button" aria-expanded="false" style={{ color: modeColor.text }}>
                                     Community
                                 </a>
                             </li>
                             <li className="nav-item ml-3">
                                 <div className="d-flex align-items-center">
-                                    <div className="nav-link active font-weight-bold" id="navbarDropdown" style={{width: "100px"}}>
-                                        Dark Mode
+                                    <div className="nav-link active font-weight-bold" id="navbarDropdown" style={{width: "110px", color: modeColor.text }} >
+                                        {theme === "light" ? "Dark" : "Light"} Mode
                                     </div>
                                     <div className="custom-control custom-switch nav-link ml-4">
-                                        <input type="checkbox" className="custom-control-input" id="customSwitch1" defaultunchecked />
+                                        <input type="checkbox" className="custom-control-input" id="customSwitch1" onClick={handleToggleTheme} defaultunchecked />
                                         <label className="custom-control-label" htmlFor="customSwitch1"></label>
                                     </div>
                                 </div>
--- src/context/themeContext.js
+++ src/context/themeContext.js
@@ -3,10 +3,7 @@ import React, { createContext, useState, useContext, useEffect} from 'react';
 
 
 // creating a context for app theme
-const ThemeContext = createContext({
-    themeMode: "light",  // theme mode status
-    toggleTheme: () => {} // changing the theme of the app  
-});
+const ThemeContext = createContext(null);
 
 // creating custom context hook to toggle the theme of the app
 export const useTheme = () => {
@@ -15,10 +12,27 @@ export const useTheme = () => {
 
 // now, creating the provider for the app
 export const ThemeProvider = (props) => {
+
+    // defining the color palletes for the theme
+    const colorPalletes = {
+        light: {  // colors for the dark mode
+            background: "#FFFFFF",
+            text: "#000000"
+        },
+        dark: {  // colors for the light mode
+            background: "#232323",
+            text: "#FFFFFF"
+        },
+    };
+
+    // now, set the theme of the app
+    const [theme, setTheme] = useState("light");  // it can be (light | dark)
+
+    // returning the context provider, so that it is accessible for all
     return (
 
         // wrapping all childrens props
-        <ThemeContext.Provider value={{  }}>
+        <ThemeContext.Provider value={{ colorPalletes, theme, setTheme }}>
             {props.children}
         </ThemeContext.Provider>
     );
