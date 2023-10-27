52ff4637fafab115c0e5fd672ff00e91ac8450f6
Hritik <pypi.pioneer@gmail.com>
2023-10-27 13:56:49 +0530

    pagination optimized

--- src/components/ImageCollection.jsx
+++ src/components/ImageCollection.jsx
@@ -1,10 +1,9 @@
 import { Grid } from '@mui/material';
-import React, { useState } from 'react';
+import React from 'react';
 import { useFetchImage } from '../context/fetchAllPics';
 import { useTheme } from '../context/themeContext';
 import ImageProfile from './ImageProfile';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import axios from 'axios';
 
 
 // it will contain the image fetch from the unsplash API
@@ -12,35 +11,9 @@ export default function ImageCollection() {
 
     // fetch the values from the theme context
     const { colorPalletes, theme } = useTheme();
-    const [modeColor, setModeColor] = useState(colorPalletes.light);
 
     // fetch all photos to display here
-    const { allImages, setAllImages } = useFetchImage();
-
-    // the page number to get the content
-    const [pageNumber, setPageNumber] = useState(2);
-
-    // now, fetching more data to implement pagination
-    const fetchMoreImages = async (req, res) => {
-
-        const val = 1;  // to increase the page number
-
-        // now, fetch the more data from the next page
-        const url = `https://api.unsplash.com/photos/?client_id=JkvPSgnalbX3LRXHuVaVdQS9wA9qyho36F-OgZZc6qY&page=${pageNumber}`;
-
-        // now, fetch the images
-        axios.get(url)
-            .then(res => {
-                setAllImages(allImages.concat(res.data));
-                setPageNumber(pageNumber + val);
-                console.log(allImages)
-            })
-            .catch(err => {  // encountered errors while fetching images
-                if (!axios.isCancel(err)) {  // request is aborted
-                    console.log(err);  // handle errors
-                }
-            });
-    };
+    const { allImages, fetchMoreImages } = useFetchImage();
 
     // display jsx content from here
     return (
@@ -49,7 +22,7 @@ export default function ImageCollection() {
             <InfiniteScroll
                 dataLength={allImages.length}
                 next={fetchMoreImages}
-                hasMore={allImages?.length < 50}  // there are alot of articles present, restricting after displaying some articles out of those
+                hasMore={allImages?.length < 500}  // there are alot of articles present, restricting after displaying some articles out of those, api is not giving the total result parameters
             >
 
                 <div style={{ margin: '40px 50px', backgroundColor: theme === "dark" ? colorPalletes.dark.background : colorPalletes.light.background }}>
--- src/context/fetchAllPics.js
+++ src/context/fetchAllPics.js
@@ -23,8 +23,7 @@ export const FetchImageProvider = (props) => {
         const cancelToken = axios.CancelToken.source();
 
         // to get the data from all pages
-        let pageNumber = 1;
-        let url = `https://api.unsplash.com/photos/?client_id=${API_KEY}&page=${pageNumber}`;
+        let url = `https://api.unsplash.com/photos/?client_id=${API_KEY}&page=1`;
 
         // now, fetch the data from the api
         axios.get(url, { cancelToken: cancelToken.token })
@@ -44,12 +43,36 @@ export const FetchImageProvider = (props) => {
         }
     }, []);
 
-    
+    // the page number to get the content
+    const [pageNumber, setPageNumber] = useState(2);
+
+    // now, fetching more data to implement pagination
+    const fetchMoreImages = async (req, res) => {
+
+        const val = 1;  // to increase the page number
+
+        // now, fetch the more data from the next page
+        const url = `https://api.unsplash.com/photos/?client_id=JkvPSgnalbX3LRXHuVaVdQS9wA9qyho36F-OgZZc6qY&page=${pageNumber}`;
+
+        // now, fetch the images
+        axios.get(url)
+            .then(res => {
+                setAllImages(allImages.concat(res.data));
+                setPageNumber(pageNumber + val);
+                console.log(allImages)
+            })
+            .catch(err => {  // encountered errors while fetching images
+                if (!axios.isCancel(err)) {  // request is aborted
+                    console.log(err);  // handle errors
+                }
+            });
+    };
+
     // return the provider and list of photos
     return (
         
         // wrapping all childrens
-        <FetchImageContext.Provider value={{ allImages, setAllImages }}>
+        <FetchImageContext.Provider value={{ allImages, setAllImages, fetchMoreImages }}>
             {props.children}
         </FetchImageContext.Provider>
     );
