a3d9ad00057b7a733b2d6534f870ce8f94fda9d2
Hritik <pypi.pioneer@gmail.com>
2023-10-27 06:13:56 +0530

    theme context created to toggle the theme mode

---
+++ src/context/themeContext.js
@@ -0,0 +1,25 @@
+// importing requriements
+import React, { createContext, useState, useContext, useEffect} from 'react';
+
+
+// creating a context for app theme
+const ThemeContext = createContext({
+    themeMode: "light",  // theme mode status
+    toggleTheme: () => {} // changing the theme of the app  
+});
+
+// creating custom context hook to toggle the theme of the app
+export const useTheme = () => {
+    return useContext(ThemeContext);
+};
+
+// now, creating the provider for the app
+export const FetchNoteProvider = (props) => {
+    return (
+
+        // wrapping all childrens props
+        <ThemeContext.Provider value={{  }}>
+            {props.children}
+        </ThemeContext.Provider>
+    );
+}
