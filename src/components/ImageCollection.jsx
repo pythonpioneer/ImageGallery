95fff3ee32127af5b40662eff823287fd9578e9d
Hritik <pypi.pioneer@gmail.com>
2023-10-27 12:52:14 +0530

    now, i need to implement fetch more data function

--- src/components/ImageCollection.jsx
+++ src/components/ImageCollection.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { useFetchImage } from '../context/fetchAllPics';
 import { useTheme } from '../context/themeContext';
 import ImageProfile from './ImageProfile';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 // it will contain the image fetch from the unsplash API
@@ -17,15 +18,25 @@ export default function ImageCollection() {
 
     // display jsx content from here
     return (
-        <div style={{ margin: '40px 50px', backgroundColor: theme === "dark" ? colorPalletes.dark.background : colorPalletes.light.background }}>
-            <Grid container spacing={4} justify="center" alignItems="center">
+        <>
+            {/* implementing pagenation */}
+            <InfiniteScroll
+                dataLength={allImages.length}
+                next={fetchMoreImages}
+                hasMore={allImages?.length < 50}  // there are alot of articles present, restricting after displaying some articles out of those
+            >
 
-                { allImages?.map((gallery, index) => {
-                    return <Grid item lg={4} xs={12} sm={6} md={6} key={gallery.urls.raw} >
+                <div style={{ margin: '40px 50px', backgroundColor: theme === "dark" ? colorPalletes.dark.background : colorPalletes.light.background }}>
+                    <Grid container spacing={4} justify="center" alignItems="center">
+
+                        {allImages?.map((gallery, index) => {
+                            return <Grid item lg={4} xs={12} sm={6} md={6} key={gallery.urls.raw} >
                                 <ImageProfile sourceImage={gallery.urls.small} actualImage={gallery.urls.raw} likeCounts={gallery.likes} username={gallery.user.username} actualName={gallery.user.name} profileImage={gallery?.user?.profile_image?.small} />
                             </Grid>
-                })}
-            </Grid>
-        </div>
+                        })}
+                    </Grid>
+                </div>
+            </InfiniteScroll>
+        </>
     )
 }
